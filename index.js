8a9aeec1d5250ddb36cd8af7544272f7d478fefd
neethupath <pathnee222gmail.com>
2023-03-30 18:37:00 -0400

    control

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+
+  if(ride <= 400)
+  {
+  return('This one is on me!');
+  }
+  else if(ride <= 2000)
+  {
+return('That will be twenty bucks.');
+  }
+  else if(ride>2000 && ride<2500)
+  {
+    return ('I will gladly take your thirty bucks.');
+  }
+  else if(ride>2500)
+  {
+  return('No can do.');
+  }
+
+}
+
+function ternaryCheckCity(city){
+  // Write your code here!
+
+  return((city === 'NYC')?('Ok, sounds good.'):('No go.'));
+
+
+}
+
+function switchOnCharmFromTip(tip){
+
+  switch(tip){
+    case 'generous':return('Thank you so much.');
+                    break;
+     case 'not as generous':return('Thank you.');
+                    break;
+       default:return('Bye.');
+
+  }
+ /* if(tip==='generous')
+  {
+  return ('Thank you so much.');
+  }
+  else if(tip!='generous')
+  {
+  return('Thank you.');
+  }
+ else if(tip===)
+  {
+  return('Bye.');
+  }*/
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
